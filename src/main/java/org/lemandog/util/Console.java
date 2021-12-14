37b6425eca776a37c3b06c51874a7ec8e8bb6e9d
Lemandog <alexo98@gmail.com>
2021-12-14 17:14:51 +0300

    -Прибавил +0.0.1 к версии

--- src/main/java/org/lemandog/util/Console.java
+++ src/main/java/org/lemandog/util/Console.java
@@ -13,7 +13,7 @@ public class Console {
     }
     public static void ready(){
         printLine('@');
-        coolPrintout("Atom simulation v 0.3.9 ");
+        coolPrintout("Atom simulation v 0.4.0 ");
         printLine('@');
     }
 
